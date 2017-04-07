23587e52b1445623454b8844ae142eb179daf8d1
StarWindI <starwind@pisem.net>
2017-04-08 01:52:25 +0300

    screen2world coordinate

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -14,7 +14,50 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetContolledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetContolledTank()) { return; }
+	FVector HitLocation;
+	if (GetSightRayHitLocation(HitLocation)) {
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString())
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLoction) const
+{
+	FVector WorldLoction;
+	int32 SizeX, SizeY;
+	GetViewportSize(SizeX, SizeY);
+	FVector2D ScreenLocation = FVector2D(CrosshairXLocation * SizeX, CrosshairYLocation * SizeY - 10);
+	DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WorldLoction, HitLoction);
+
+	HitLoction *= 20000;
+	FVector Position;
+	FRotator Rotattion;
+	GetPlayerViewPoint(OUT Position, OUT Rotattion);
+	FVector TraceEnd = Position + Rotattion.Vector() * 1000;
+
+	DrawDebugLine(
+		GetWorld(),
+		TraceEnd,
+		HitLoction,
+		FColor(255, 0, 0),
+		false,
+		0.0f,
+		0.0f,
+		2.0f
+	);
+
+	return true;
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -12,7 +12,15 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
-	ATank* GetContolledTank() const;
-
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	ATank* GetContolledTank() const;
+	void AimTowardsCrosshair();
+	bool GetSightRayHitLocation(FVector& HitLoction) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5f;
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.33333f;
 };
