86dd6680cca14a01961cdc7a826616081047752e
StarWindI <starwind@pisem.net>
2017-04-08 16:49:25 +0300

    Aim Component

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -9,14 +9,13 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
@@ -35,17 +34,6 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation, FColor TraceColor) const
 {
-	FVector StartLocation = GetActorLocation();
-	DrawDebugLine(
-		GetWorld(),
-		StartLocation,
-		HitLocation,
-		TraceColor,
-		false,
-		0.0f,
-		0.0f,
-		3.0f
-	);
-	//	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *GetName(), *HitLocation.ToString())
+	TankAimingComponent->AimAt(HitLocation, TraceColor);
 }
 
--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
 
@@ -10,15 +11,16 @@ class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
 public:
 	// Sets default values for this pawn's properties
 	ATank();
 
-protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
@@ -26,4 +28,5 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	void AimAt(FVector HitLocation, FColor TraceColor) const;
+
 };
