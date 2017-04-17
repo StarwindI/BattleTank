d28f2a8129c514c9526506eaa602bf3c3ccb8e35
StarwindI <starwind@pisem.net>
2017-04-17 15:45:44 +0300

    correct force

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -4,6 +4,8 @@
 void UTankTrack::SetThrottle(float Throttle) {
 	FVector ForceApplied = Throttle * GetForwardVector() * TrackMaxDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
+	// TODO find actual altitude Z
+	ForceLocation.Z += 100.0f;
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
